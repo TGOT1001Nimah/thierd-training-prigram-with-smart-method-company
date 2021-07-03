711ae5a7da9dd18abdff63e858198543020d9a32
Nimah Alyamani <66944123+TGOT1001Nimah@users.noreply.github.com>
2021-07-03 10:31:02 +0300

    6 motors in 90 to 0 degrees and to 90 degree
    you will find a video and the code also the circuit to controlling 5 servo motors,  but i add more one first will see how the motors will change from 90 degrees to 0 then i change the code to make it in a 90-degree position. this is for the third training program with a smart method company.

---
+++ _5_servo_motor_to_90_degree_to_0t.ino
@@ -0,0 +1,45 @@
+/* Servo motor with Arduino example code. Position and sweep. More info: https://www.makerguides.com/ */
+
+// Include the servo library:
+#include <Servo.h>
+
+// Create a new servo object:
+
+Servo s13, s12, s11, s10, s9, s8;
+
+// Define the servo pin:
+#define servoPin 13, 12. 10, 9, 8
+
+
+void setup() {
+  // Attach the Servo variable to a pin:
+  s13.attach( 13);
+  s12.attach( 12);
+  s11.attach( 11);
+  s10.attach( 10);
+ s9.attach( 9);
+  s8.attach( 8);
+}
+
+void loop() {
+  // Tell the servo to go to a particular angle:
+  //90 degree position
+   s13.write(90);
+  s12.write(90);
+  s11.write(90);
+  s10.write(90);
+  s9.write(90);
+  s8.write(90);
+  delay(1000);
+//return it to 0 position
+     s13.write(0);
+  s12.write(0);
+  s11.write(0);
+  s10.write(0);
+  s9.write(0);
+  s8.write(0);
+  delay(1000);
+
+
+  delay(1000);
+}
